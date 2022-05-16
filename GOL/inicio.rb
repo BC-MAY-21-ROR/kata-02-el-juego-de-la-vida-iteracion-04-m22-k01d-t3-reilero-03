aeca77fc78f678923ddc91a6ca6feb5e528a1ffd
EdwardMD <Edwardmi444@gmail.com>
2022-05-16 18:46:29 -0500

    Generations update
    -Correction on the loop section
    -We add a new user get called "generations" to indicate the limit of generations that are going to be created.
    
    Co-authored-by: @Jumaquba <jumaquba@gmail.com>
    Co-authored-by:  @V-Juarez <mytacam78@gmail.com>

--- GOL/inicio.rb
+++ GOL/inicio.rb
@@ -11,22 +11,26 @@ class Iniciar
     print "Elige el alto del tablero: "
     $height = gets.chomp.to_i
 
-    fm=Creator.new.create_first_matrix($height,$width)
-    Display.new.display_matrix(fm)
-
-    nm=Neighbours.new.find_neighbour(fm)
+    print "Ingrese la cantidad de generaciones desada: "
+    $generation = gets.chomp.to_i    
+ 
     #Display.new.display_matrix(nm)
     
-    #ciclo infinito
-    loop do
-       fm=nm
-       nm=Neighbours.new.find_neighbour(fm)
-       Display.new.display_matrix(nm)
-       sleep(1)
-    end
-
-
-
+    puts "Condicion inicial/generacion 0 \n"
+    puts "Alto: #{$height} - Ancho: #{$width} \n"
+    fm=Creator.new.create_first_matrix($height,$width)
+    Display.new.display_matrix(fm)
     
+    nm=Neighbours.new.find_neighbour(fm)
+    
+    for generation_count in 1..$generation do
+      puts "Generacion #{generation_count}: \n"
+      puts "Alto: #{$height} - Ancho: #{$width} \n"
+      fm=nm
+      nm=Neighbours.new.find_neighbour(fm)
+      Display.new.display_matrix(nm)
+      sleep(1)
+      generation_count=generation_count+1
+    end
   end
 end
